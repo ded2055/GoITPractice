1cfc161ecd6b35ca60c907c6390c706de2922745
ded2055 <ded2055@yandex.ru>
2016-05-01 12:41:15 +0300

    Create task2_3

---
+++ AverageNumber.java
@@ -0,0 +1,11 @@
+package goit.task2_3;
+
+
+public class AverageNumber {
+    public int average(int a, int b) {
+        long aLong = a;
+        long bLong = b;
+
+        return (int)((aLong+bLong)/2);
+    }
+}
